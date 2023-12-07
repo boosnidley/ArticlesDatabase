14b144a6086cc0fd1bc45d5868a39fe5e339824d
Tyler Hoover <80370906+tjayhoover@users.noreply.github.com>
2023-12-07 14:52:00 -0500

    Updated gitignore and application file

--- application.py
+++ application.py
@@ -1,19 +1,4 @@
-from pymongo import MongoClient
-from connection_string import connection_string
-
-
-def get_database():
- 
-   # Provide the mongodb atlas url to connect python to mongodb using pymongo
-   CONNECTION_STRING = connection_string
-   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-   client = MongoClient(CONNECTION_STRING)
- 
-   # Create the database for our example (we will use the same database throughout the tutorial
-   return client['user_shopping_list']
-  
-# This is added so that many files can reuse the function get_database()
-if __name__ == "__main__":   
-  
-   # Get the database
-   dbname = get_database()
+# Get the database using the method we defined in pymongo_test_insert file
+from pymongo_get_database import get_database
+dbname = get_database()
+collection_name = dbname["articles"]
