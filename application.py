f616918909fc23b376c7efe6386b694596de2b7f
iacarpenter <90263677+iacarpenter@users.noreply.github.com>
2023-12-13 20:13:53 -0500

    Update application.py with publisher_query

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from pymongo_get_database import get_database
 # Import the queries
 from queries.TylerQuery import date_query
 from queries.BradyQuery import tag_query
+from queries.IanQuery import publisher_query
 
 # Initialize database connection
 dbname = get_database()
@@ -14,6 +15,7 @@ while(True):
     print("Welcome to NewsQuery! Choose one of the following options:")
     print("(1) Query news articles by date.")
     print("(2) Query news articles by topic.")
+    print("(3) Query news articles by publisher.")
     print("(q) Quit.")
 
     response = input()
@@ -28,6 +30,12 @@ while(True):
         topic = input("Enter the topic you want to search by\n")
         try:
            tag_query(articles, topic)
+        except:
+            print("Failure. Ensure you have a network connection.")
+    elif(response == '3'):
+        publisher = input("Enter the publisher you want to search by\n")
+        try:
+           publisher_query(articles, publisher)
         except:
             print("Failure. Ensure you have a network connection.")
     elif(response =='q'):
