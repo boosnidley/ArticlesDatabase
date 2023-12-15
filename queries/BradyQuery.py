0977627306a52377d1a814b91d9b077b55067e53
Tyler Hoover <80370906+tjayhoover@users.noreply.github.com>
2023-12-14 21:16:09 -0500

    Made queries case insensitive

--- queries/BradyQuery.py
+++ queries/BradyQuery.py
@@ -1,6 +1,6 @@
 from helper import print_articles
 
 def tag_query(container , tag):
-  tagQuery = { "tags": tag }
+  tagQuery = { "tags": { "$regex": tag, "$options": "i" } }
   docs = container.find(tagQuery)
   print_articles(docs)
--- queries/IanQuery.py
+++ queries/IanQuery.py
@@ -1,6 +1,6 @@
 from helper import print_articles
 
 def publisher_query(container, publisher):
-  publisherQuery = { "publisher": publisher }
+  publisherQuery = { "publisher": { "$regex": publisher, "$options": "i" } }
   docs = container.find(publisherQuery)
   print_articles(docs)
